9c57447d098e6958723de744af328b785ebfdb3a
Jack Grigg <thestr4d@gmail.com>
2021-11-28 23:48:27 +0000

    Set up binary option parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
+use std::io;
+
+use gumdrop::Options;
+
 mod plugin;
 mod proxy;
 
-fn main() {
-    println!("Hello, world!");
+const BINARY_NAME: &str = "age-plugin-remote";
+
+#[derive(Debug, Options)]
+struct PluginOptions {
+    #[options(help = "Print this help message and exit.")]
+    help: bool,
+
+    #[options(help = "Print version info and exit.", short = "V")]
+    version: bool,
+
+    #[options(
+        help = "Run the given age plugin state machine. Internal use only.",
+        meta = "STATE-MACHINE",
+        no_short
+    )]
+    age_plugin: Option<String>,
+
+    #[options(help = "Expose the identity file at IDENTITY. May be repeated.")]
+    identity: Vec<String>,
+
+    #[options(
+        help = "SSH destination to proxy identity files to. May be repeated.",
+        free
+    )]
+    destination: Vec<String>,
+}
+
+fn main() -> io::Result<()> {
+    env_logger::builder()
+        .format_timestamp(None)
+        .filter_level(log::LevelFilter::Off)
+        .parse_default_env()
+        .init();
+
+    let opts = PluginOptions::parse_args_default_or_exit();
+
+    if let Some(state_machine) = opts.age_plugin {
+        todo!("Run plugin state machine");
+    } else if opts.version {
+        println!("{} {}", BINARY_NAME, env!("CARGO_PKG_VERSION"));
+    } else if opts.identity.is_empty() {
+        eprintln!("At least one age identity must be specified to expose.");
+    } else if opts.destination.is_empty() {
+        eprintln!("At least one SSH destination must be specified to proxy identities to.");
+    } else {
+        todo!("Run proxy");
+    }
+
+    Ok(())
 }
